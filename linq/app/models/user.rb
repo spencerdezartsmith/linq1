2d57cdfd93692ba100823c5dba2d9e768f7aa47a
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-08-13 16:31:17 -0700

    Add custom #contacts method to the user model

--- linq/app/models/user.rb
+++ linq/app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
-  has_many :requested_contacts, foreign_key: :requested_id, class_name: 'Contact'
+  has_many :requested_contacts, foreign_key: :requester_id, class_name: 'Contact'
   has_many :accepted_contacts, foreign_key: :acceptor_id, class_name: 'Contact'
-  has_many :contacts, through: :accepted_contacts, source: :requester
+
+  def contacts
+    contacts = []
+    arr = requested_contacts.pluck(:acceptor_id, :requester_id) + accepted_contacts.pluck(:acceptor_id, :requester_id)
+    if !arr.blank?
+      arr.flatten!.uniq!
+      arr.delete(self.id)
+      User.where(id: arr)
+    else
+      contacts
+    end
+  end
 end

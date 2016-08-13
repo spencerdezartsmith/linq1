e7f9028ca1762044ae069a1a70b3b72d2e7050ba
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-08-13 12:40:25 -0700

    Add associations to User model

--- linq/app/models/user.rb
+++ linq/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :requested_contacts, foreign_key: :acceptor_id, class_name: 'Contact'
+  has_many :accepted_contacts, foreign_key: :requester_id, class_name: 'Contact'
 end

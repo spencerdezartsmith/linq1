9cfa2cde40d29ca0811c65856b3a4f2e9a5e6eb6
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-08-13 12:56:54 -0700

    Change associations in User

--- linq/app/models/user.rb
+++ linq/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :requested_contacts, foreign_key: :acceptor_id, class_name: 'Contact'
-  has_many :accepted_contacts, foreign_key: :requester_id, class_name: 'Contact'
+  has_many :requested_contacts, foreign_key: :requested_id, class_name: 'Contact'
+  has_many :accepted_contacts, foreign_key: :acceptor_id, class_name: 'Contact'
+  has_many :contacts, through: :accepted_contacts, source: :requester
 end

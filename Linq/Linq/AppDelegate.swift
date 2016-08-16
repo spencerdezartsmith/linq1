593d7c7ed384e42b869ff4a1fb783d312662849c
Kaitlyn Adelaide <email": null>
2016-08-16 10:57:10 -0700

    Add login page that appears before tabbed views

--- Linq/Linq/AppDelegate.swift
+++ Linq/Linq/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let loginVC = storyboard.instantiateViewControllerWithIdentifier("LoginVC") as! LoginViewController
+        self.window?.rootViewController = loginVC
         return true
     }
 

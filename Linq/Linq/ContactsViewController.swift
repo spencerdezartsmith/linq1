3a5d32cbb82a536639cbf341d2e790b687f49d1a
Kaitlyn Adelaide <email": null>
2016-08-15 23:33:37 -0700

    Remove react native project and add page structure for swift front end

---
+++ Linq/Linq/ContactsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.swift
+//  Linq
+//
+//  Created by Apprentice on 8/15/16.
+//  Copyright © 2016 Linq Team. All rights reserved.
+//
+
+import Cocoa
+
+class ContactsViewController: UITableViewController {
+
+}
